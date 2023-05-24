cb0534ec535e15b97fa61fe28718b90800085dfb
LeShib <dibernardonikko@gmail.com>
2023-05-24 14:57:46 +0200

    remaining css

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # weather-app
+
+An application presenting weather information such as temperatures, humidity, wind speed and pressure for one or more cities chosen (and entered in the input field) by the user, displaying the weather and a graph of the maximum temperatures and minimum over five days (with today's date included).
+
+## 🛠 Technos
+
+<p align="center">
+  <a href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=js,html,css,sass,git" />
+  </a>
+</p>
+<img src="https://img.shields.io/badge/Technos-HMTL5_/_SCSS_/_CSS3_/_JS_/_Git-green?style=for-the-badge&logo=appveyor" alt="Badge Technologies" style="margin-right:10px;">
+
+## Rendu et lien
+
+---
+https://leshib.github.io/weather-app/
+
+<a href="https://becode.org"><img src="https://img.shields.io/badge/Project-BeCode-blue?style=for-the-badge&logo=appveyor" alt="Badge Project" style="margin-right:10px;">
+</a>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,195 @@
-// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-// https://api.openweathermap.org/data/2.5/forecast?lat=51.5073219&lon=-0.1276474&appid=e9e7d5e383492b6701141837a3db35df
-
+// Variables
 const weatherForm = document.getElementById('weatherForm');
 const cityInput = document.getElementById('cityInput');
 const weatherData = document.getElementById('weatherData');
 const apiKey = "e9e7d5e383492b6701141837a3db35df";
+const cities = [
+    "Abou Dhabi",
+    "Abuja",
+    "Accra",
+    "Addis-Abeba",
+    "Alger",
+    "Amman",
+    "Amsterdam",
+    "Andorre-la-Vieille",
+    "Antananarivo",
+    "Apia",
+    "Achgabat",
+    "Athènes",
+    "Bagdad",
+    "Bakou",
+    "Bamako",
+    "Bandar Seri Begawan",
+    "Bangkok",
+    "Bangui",
+    "Banjul",
+    "Basseterre",
+    "Belgrade",
+    "Belmopan",
+    "Berlin",
+    "Berne",
+    "Bichkek",
+    "Bissau",
+    "Bogota",
+    "Brazzaville",
+    "Bridgetown",
+    "Brisbane",
+    "Brasília",
+    "Bratislava",
+    "Brazzaville",
+    "Bridgetown",
+    "Brisbane",
+    "Brasília",
+    "Bratislava",
+    "Brazzaville",
+    "Bridgetown",
+    "Brisbane",
+    "Brasília",
+    "Bratislava",
+    "Bruxelles",
+    "Bucarest",
+    "Budapest",
+    "Bujumbura",
+    "Caracas",
+    "Castries",
+    "Chisinau",
+    "Cité du Vatican",
+    "Conakry",
+    "Copenhague",
+    "Dacca",
+    "Dakar",
+    "Damas",
+    "Djibouti",
+    "Dodoma",
+    "Douchanbé",
+    "Dublin",
+    "Freetown",
+    "Funafuti",
+    "Georgetown",
+    "Gaborone",
+    "Guatemala",
+    "Hanoï",
+    "Harare",
+    "Helsinki",
+    "Honiara",
+    "Islamabad",
+    "Jakarta",
+    "Jérusalem",
+    "Kaboul",
+    "Kampala",
+    "Khartoum",
+    "Kigali",
+    "Kingston",
+    "Kingstown",
+    "Koweït",
+    "La Havane",
+    "La Paz",
+    "La Valette",
+    "Libreville",
+    "Lilongwe",
+    "Lima",
+    "Lisbonne",
+    "Ljubljana",
+    "Londres",
+    "Lomé",
+    "Luanda",
+    "Lusaka",
+    "Luxembourg",
+    "Madrid",
+    "Majuro",
+    "Malabo",
+    "Malé",
+    "Maputo",
+    "Maseru",
+    "Masqat",
+    "Mbabane",
+    "Minsk",
+    "Mexico",
+    "Monaco",
+    "Monrovia",
+    "Montevideo",
+    "Moroni",
+    "Moscou",
+    "N'Djaména",
+    "Nairobi",
+    "Nassau",
+    "Ngerulmud",
+    "Niamey",
+    "Nicosie",
+    "Nouakchott",
+    "Nouméa",
+    "Noursoultan",
+    "Ottawa",
+    "Oslo",
+    "Oulan-Bator",
+    "Ouagadougou",
+    "Paris",
+    "Pékin",
+    "Podgorica",
+    "Port-au-Prince",
+    "Port-Louis",
+    "Port Moresby",
+    "Porto-Novo",
+    "Port-Vila",
+    "Prague",
+    "Praia",
+    "Pristina",
+    "Pyongyang",
+    "Qatar",
+    "Quito",
+    "Rangoon",
+    "Reykjavik",
+    "Riga",
+    "Riyad",
+    "Rome",
+    "Roseau",
+    "Saint-Domingue",
+    "Saint George's",
+    "Saint John's",
+    "Sainte-Lucie",
+    "San José",
+    "San Marino",
+    "San Salvador",
+    "Sanaa",
+    "Santiago",
+    "Sarajevo",
+    "Skopje",
+    "Sofia",
+    "São Tomé",
+    "Tachkent",
+    "Tallinn",
+    "Tarawa-Sud",
+    "Tbilissi",
+    "Tegucigalpa",
+    "Téhéran",
+    "Thimphou",
+    "Tirana",
+    "Tokyo",
+    "Tripoli",
+    "Tunis",
+    "Ulan-Bator",
+    "Vaduz",
+    "Varsovie",
+    "Victoria",
+    "Vientiane",
+    "Vilnius",
+    "Washington D.C.",
+    "Wellington",
+    "Windhoek",
+    "Yaoundé",
+    "Yaren",
+    "Zagreb"
+]
 
+// Fonctions 
+// Récupération de la(des) valeur(s) de l'input
 function getCity(event) {
     event.preventDefault();
     let cities = cityInput.value.split(",");
     cities.forEach(city => getWeatherData(city.trim()));
 }
 
+// Récupération des informations à propos de la(des) ville(s) choisie(s)
 async function getWeatherData(city) {
     let dataFetch = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
     let data = await dataFetch.json();
@@ -21,6 +198,7 @@ async function getWeatherData(city) {
     displayWeather(city, latitude, longitude);
 }
 
+// Création et affichage de l'HTML pour les infos de météo
 function displayWeather(city, lat, lon) {
     let weatherFetch = fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
     weatherFetch.then((response) => response.json())
@@ -39,7 +217,6 @@ function displayWeather(city, lat, lon) {
             let date4 = fullDate.getFullYear() + "-0" + (fullDate.getMonth()+1) + "-" + (fullDate.getDate()+4);
             let fiveDaysDates = [];
             fiveDaysDates.push(date0, date1, date2, date3, date4);
-            console.log(fiveDaysDates);
             let temperatureByDay = [];
             let maxTemp = dataArray[0].main.temp_max;
             let minTemp = dataArray[0].main.temp_min;
@@ -77,8 +254,7 @@ function displayWeather(city, lat, lon) {
                 weatherDiv.innerHTML = `${temperatureByDay[j].date}:<br>
                 <img src="https://openweathermap.org/img/wn/${icon}@2x.png"><br>
                 Weather: ${dataArray[i].weather[0].description}<br> 
-                Temperature min: ${temperatureByDay[j].min}°C<br>
-                Temperature max: ${temperatureByDay[j].max}°C<br>
+                Temperatures : ${temperatureByDay[j].min}°C  -   ${temperatureByDay[j].max}°C<br>
                 Humidity: ${dataArray[i].main.humidity}%<br>
                 Pressure: ${dataArray[i].main.pressure}Pa<br>
                 Wind speed: ${dataArray[i].wind.speed}Km/h`
@@ -89,6 +265,7 @@ function displayWeather(city, lat, lon) {
         })
 }
 
+// Création du graphique des température min et max
 function createChart(city, temperatureData, labels) {
     const chartCanvas = document.createElement('canvas');
     chartCanvas.setAttribute('id', `${city}-chart`);
@@ -124,8 +301,120 @@ function createChart(city, temperatureData, labels) {
     });
 }
 
+/* Fonction d'auto-complétion 
+Merci https://www.w3schools.com/howto/howto_js_autocomplete.asp */
+function autocomplete(inp, arr) {
+    let currentFocus;
+  
+    // Gère l'événement "input" lorsque l'utilisateur écrit dans le champ de texte.
+    function handleInput(e) {
+        let val = this.value;
+        closeAllLists();
+        if(!val) { 
+            return false;
+        }
+        currentFocus = -1;
+        let a = createAutocompleteContainer();
+        for(let i = 0; i < arr.length; i++) {
+            if (startsWith(arr[i], val)) {
+                let b = createAutocompleteItem(arr[i], val);
+                b.addEventListener("click", handleItemClick);
+                a.appendChild(b);
+            }
+        }
+    }
+  
+    // Gère l'événement "keydown" lorsque l'utilisateur appuie sur une touche du clavier.
+    function handleKeyDown(e) {
+        let x = getAutocompleteItems();
+        if(e.keyCode == 40) {
+            currentFocus++;
+            addActive(x);
+        }else if(e.keyCode == 38) {
+            currentFocus--;
+            addActive(x);
+        }else if(e.keyCode == 13) {
+            e.preventDefault();
+        if(currentFocus > -1) {
+            if (x) x[currentFocus].click();
+        }
+      }
+    }
+  
+    // Gère l'événement "click" lorsque l'utilisateur clique sur le document.
+    function handleClick(e) {
+        closeAllLists(e.target);
+    }
+  
+    // Crée et retourne un élément div pour contenir les éléments de l'autocomplétion.
+    function createAutocompleteContainer() {
+        let a = document.createElement("div");
+        a.setAttribute("id", inp.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        inp.parentNode.appendChild(a);
+        return a;
+    }
+  
+    // Crée et retourne un élément div pour chaque élément correspondant de l'autocomplétion.
+    function createAutocompleteItem(item, val) {
+        let b = document.createElement("div");
+        b.innerHTML = "<strong>" + item.substr(0, val.length) + "</strong>";
+        b.innerHTML += item.substr(val.length);
+        b.innerHTML += "<input type='hidden' value='" + item + "'>";
+        return b;
+    }
+  
+    // Gère l'événement "click" lorsqu'un élément d'autocomplétion est cliqué.
+    function handleItemClick(e) {
+        inp.value = this.getElementsByTagName("input")[0].value;
+        closeAllLists();
+    }
+  
+    // Ajoute la classe "autocomplete-active" à l'élément actuellement sélectionné.
+    function addActive(x) {
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+  
+    // Supprime la classe "autocomplete-active" de tous les éléments d'autocomplétion
+    function removeActive(x) {
+        for (let i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+  
+    // Ferme toutes les listes d'autocomplétion dans le document, sauf celle passée en argument.
+    function closeAllLists(elmnt) {
+        let x = document.getElementsByClassName("autocomplete-items");
+        for (let i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+    // Vérifie si une chaîne commence par les mêmes lettres que la valeur du champ de texte, sans tenir compte de la casse.
+    function startsWith(str, val) {
+        return str.substr(0, val.length).toUpperCase() == val.toUpperCase();
+    }
+
+    // Récupère tous les éléments d'autocomplétion.
+    function getAutocompleteItems() {
+        let x = document.getElementById(inp.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        return x;
+    }
+
+    // Evenement
+    inp.addEventListener("input", handleInput);
+    inp.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("click", handleClick);
+}
+  
+// Appels de fonctions
+autocomplete(document.getElementById("cityInput"), cities);
+
+// Evenements
 weatherForm.addEventListener('submit', getCity);
-
-
-// Comparer chaque jour la température max et la température min 
-// Mise en forme de l'affichage 
